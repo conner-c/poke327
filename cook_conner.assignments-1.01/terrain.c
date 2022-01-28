0b37eb723d1a4539f522f9159d76475618f4f43b
Conner <cook.r.conner@gmail.com>
2022-01-28 00:52:46 +0000

    Created map struct, and methods to print map, initialize map, and seed map. With enum to describe different aspects of the map style and different terrian types.

--- cook_conner.assignments-1.01/terrain.c
+++ cook_conner.assignments-1.01/terrain.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
 
 #define X 80
 #define Y 21
@@ -8,7 +10,19 @@ enum terrain_type
 {
 	tall_grass,
 	boulders,
-	clearing
+	clearing,
+	sand,
+	mixed,
+	tree,
+	rock,
+	debug
+};
+
+enum region_combination{
+	c2t2s0m1,
+	c2t2s0m2,
+	c2t2s1m1,
+	c2t2s1m2
 };
 
 typedef struct map
@@ -21,11 +35,14 @@ typedef struct map
 }map_t;
 
 void printMap(map_t *map){
-	
-	for(int row = 0; row < Y; row++){
-		for(int col = 0; col < X; col++){
-			printf("I am working before switch statement");
-			switch (map->map_grid[col][row]){
+	// Creating variables
+	int row, col;
+
+	//Printing the map to terminal by going through each cell within map_grid and printing the corresponding character to stored enum
+	for(row = 0; row < Y; row++){
+		for(col = 0; col < X; col++){
+			enum terrain_type cell = map->map_grid[row][col];
+			switch (cell){
 				case tall_grass:
 					printf(",");
 					break;
@@ -36,7 +53,7 @@ void printMap(map_t *map){
 					printf(".");
 					break;
 				default:
-					printf("");
+					printf(" ");
 					break;
 			}
 		}
@@ -58,10 +75,67 @@ void init_map(map_t *map){
 
 	for(row = 1; row < Y - 1; row++){
 		for(col = 1; col < X - 1; col++){
-			map->map_grid[row][col] = clearing;
+			map->map_grid[row][col] = debug;
 		}
 	}
 } 
+
+
+int my_rand(int lower_bound, int upper_bound){
+	srand(time(NULL));
+	return rand() % upper_bound + lower_bound;
+}
+
+void place_seed(enum region_combination, map_t *map){
+	// Used to check if all regions have been seeded
+	bool clearing_region, tall_grass_region, sand_region, mixed_region;
+	int type_of_region;
+	clearing_region = tall_grass_region = sand_region = mixed_region = false;
+
+	// Placing seeds for regions
+	while(!clearing_region && !tall_grass_region && !sand_region && !mixed_region){
+		//
+		type_of_region = my_rand(0, 4);
+	}
+}
+
+
+void seed_map(map_t *map){
+	// Determining style of map to create
+	int number_of_regions, region_comb;
+	enum region_combination region_combi;
+
+
+	// Getting random value
+	region_comb = my_rand(0, 100);
+
+	// Determining map style
+	if(region_comb >= 0 && region_comb <= 27){
+		number_of_regions = 5;
+		region_combi = c2t2s0m1;
+	}
+	else if(region_comb >= 28 && region_comb <= 49){
+		number_of_regions = 6;
+		region_combi = c2t2s0m2;
+	}
+	else if(region_comb >= 50 && region_comb <= 77){
+		number_of_regions = 6;
+		region_combi = c2t2s1m1;
+	}
+	else{
+		number_of_regions = 7;
+		region_combi = c2t2s1m2;
+	}
+
+
+	place_seed(region_combi, map);
+
+
+
+
+}
+
+
 
 int main(int argc, char const *argv[])
 {
